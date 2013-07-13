83ffd593006f35acec60253c5c8c914e128822c8
robin <robinskilling@gmail.com>
2013-07-13 15:41:25 +0200

    fix show of like button on project page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -7,9 +7,9 @@ module ProjectsHelper
   def participates_in_list?
     if signed_in? && !current_user_project_owner?
       if current_user.participations.find_by_project_id(@project).nil?
-        link_to raw("<span data-icon = 'N' class='big-icons'></span"), join_project_path(@project), :id=> "participation", :method=> :put, :remote => true, :data=>{:status=>"join", :project => @project.id}
+        link_to raw("<span data-icon = 'N' class='big-icons'></span"), join_project_path(@project), :alt=>"join", :id=> "participation", :method=> :put, :remote => true, :data=>{:status=>"join", :project => @project.id}
       else
-        link_to raw("<span data-icon = 'O' class='big-icons'></span>"), leave_project_path(@project), :id=> "participation", :method=> :put, :remote => true, :data=>{:status=>"leave", :project=> @project.id}
+        link_to raw("<span data-icon = 'O' class='big-icons'></span>"), leave_project_path(@project), :alt=>"leave", :id=> "participation", :method=> :put, :remote => true, :data=>{:status=>"leave", :project=> @project.id}
       end
      end
 
