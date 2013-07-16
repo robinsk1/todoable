990533812dff16f300d6d5ee6518005e2560b394
robin <robinskilling@gmail.com>
2013-07-16 18:44:23 +0200

    ajaxified joining of list

--- app/assets/javascripts/participations.js.coffee
+++ app/assets/javascripts/participations.js.coffee
@@ -18,19 +18,23 @@ $(document).ready ->
         method = link.attr("data-status")
         new_status = ((if method is "join" then "leave" else "join"))
         new_icon = ((if method is "join" then "O" else "N"))
-        form_path = "/lists/"+project_id+"/join"
+        form_path = "/lists/"+project_id+"/join.js"
         type = ((if method is "join" then "POST" else "DELETE"))
 
         $.ajaxSetup
           url: form_path
           type: type
           cache: false
-          dataType: "json"
+          dataType: 'script'
 
         $.ajax
-          complete: ->
+          complete: (data)->
             link.attr("data-status", new_status)
             link.children('span').attr("data-icon", new_icon)
+#            $('#todo-list-partial').effect("highlight", {}, 3000);
+            eval(data)
+
+
 
 
 
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,10 +1,11 @@
 class ParticipationsController < ApplicationController
 
   def create
+     common
      participation = current_user.participations.build(:project_id => params[:id])
         respond_to do |format|
           if participation.save
-            format.json { render :nothing => true }
+            format.js {}
           else
             format.json { render json: participation.errors, status: :unprocessable_entity }
           end
@@ -13,11 +14,26 @@ class ParticipationsController < ApplicationController
 
 
    def destroy
+     common
      participation = current_user.participations.find_by_project_id(params[:id])
      participation.destroy
         respond_to do |format|
-          format.json { render :nothing => true }
+          format.js{}
         end
-     end
+   end
+
+  private
+
+  def common
+      @project = Project.find(params[:id])
+      @closed = Project.find(@project).todos.joins(:completes).where("completes.user_id" => current_user)
+      @open = Project.find(@project).todos
+      group_ids = @closed.map(&:id)
+      @open = Project.find(@project).todos.where(['id not in (?)', group_ids]) unless group_ids.empty?
+      @open.all
+      if current_user
+        @todo = @project.todos.build
+      end
+  end
 
 end
