82c4e7e2cf735c4569b2fa9a5c5aa63c632656d6
robin <robinskilling@gmail.com>
2013-07-25 16:26:31 +0200

    fix users likes on user home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @completes = @user.completes
     @joined = @user.joinups
+    @likes = Todo.joins(:likes).where(['likes.user_id = ?', @user.id])
   end
 
 end
